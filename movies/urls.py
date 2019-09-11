c3be3fbcd77e3d78202965a8a9cb85893ee4d976
freemanpd <patrick.freeman@sftech.io>
2019-09-11 01:08:19 -0400

    updating codebase

--- movies/urls.py
+++ movies/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import apiView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', apiView),
 ]
