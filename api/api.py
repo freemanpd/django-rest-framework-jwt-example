7082c9285012151ed551b1733cf69a3baca38e4e
freemanpd <patrick.freeman@sftech.io>
2019-09-22 00:58:15 -0400

    updating

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Movie
 from .serializers import MovieSerializer
+# from rest_framework.response import Response
 
 # Viewset
 class MovieViewSet(viewsets.ModelViewSet):
--- api/models.py
+++ api/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(blank=False, max_length=100)
-    realeased = models.IntegerField(blank=False)
-    runtime = models.IntegerField(blank=False)
+    released = models.IntegerField(blank=False)
     genre = models.CharField(blank=False, max_length=100)
-    rated = models.CharField(blank=False, max_length=10)
-    plot = models.CharField(blank=False, max_length=1000)
-    date_created = models.DateTimeField(auto_now_add=True, blank=True) 
-    last_updated = models.DateTimeField(auto_now=True, blank=True)
     creator = models.ForeignKey('auth.User', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.title
